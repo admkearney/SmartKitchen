79a3606861270a998a5c968d4f559fabcb19f1f7
Eamon624 <Eamon624@hotmail.com>
2017-04-13 15:29:44 +0100

    Stove Starting Point

---
+++ src/main/java/services/StoveService.java
@@ -0,0 +1,57 @@
+package services;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import serviceui.ServiceUI;
+
+/**
+ * The Class StoveService.
+ */
+public class StoveService extends Service {
+
+    private final Timer timer;
+    private int percentHot;
+
+    public StoveService(String name) {
+        super(name, "_stove._udp.local.");
+        timer = new Timer();
+        percentHot = 0;
+        ui = new ServiceUI(this, name);
+    }
+
+    @Override
+    public void performAction(String a) {
+        if (a.equals("get_status")) {
+            sendBack(getStatus());
+        } else if (a.equals("Cook")) {
+            timer.schedule(new RemindTask(), 0, 2000);
+            sendBack("OK");
+            ui.updateArea("Starting stove");
+        } else {
+            sendBack(BAD_COMMAND + " - " + a);
+        }
+    }
+
+    class RemindTask extends TimerTask {
+
+        @Override
+        public void run() {
+            if (percentHot < 100) {
+                percentHot += 10;
+            }
+        }
+    }
+
+    @Override
+    public String getStatus() {
+        return "Stove is " + percentHot + "% finished.";
+    }
+
+    public static void main(String[] args) {
+        new StoveService("Stove");
+    }
+}
+
+
+
